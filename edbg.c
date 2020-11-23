b92bf29c77f197ce57a18ddccb593405f0e910b2
ooxi <violetland@mail.ru>
2020-11-23 16:50:10 +0100

    GitHub Actions...

--- edbg.c
+++ edbg.c
@@ -25,7 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-this is an error
+
 /*- Includes ----------------------------------------------------------------*/
 #include <time.h>
 #include <stdio.h>
