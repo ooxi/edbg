75b4a4abb2ed5d2f5590ced96d3105763408558d
ooxi <violetland@mail.ru>
2020-11-23 16:39:05 +0100

    GitHub Actions...

--- .github/workflows/linux-amd64-clang.sh
+++ .github/workflows/linux-amd64-clang.sh
@@ -6,3 +6,10 @@
 # The following directories should be provided from the outside world
 #  - /github/workspace: read/write repository checkout
 
+dnf install -y glibc-locale-source
+localedef  --force --inputfile=en_US --charmap=UTF-8 en_US.UTF-8
+
+sudo dnf -y install	\
+	clang		\
+	make		\
+	libudev-devel
