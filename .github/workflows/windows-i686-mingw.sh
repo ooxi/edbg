c125ea73261f4b51c1db5cb08705391c00edf9c7
ooxi <violetland@mail.ru>
2020-11-23 17:32:26 +0100

    GitHub Actions...

---
+++ .github/workflows/windows-i686-mingw.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Assumes to be executed with root permissions inside a Fedora
+# container.
+#
+# Expects the repository to be available at $PWD
+
+dnf install -y glibc-locale-source
+localedef  --force --inputfile=en_US --charmap=UTF-8 en_US.UTF-8
+
+# Install build dependencies
+sudo dnf -y install	\
+	git		\
+	make		\
+	mingw64-gcc
+
+# Driver Development Kit
+git clone --branch=v2.5 --depth=1 https://github.com/uic-evl/omicron.git omicron;
+
+# Build environment
+export COMPILER='x86_64-w64-mingw32-gcc -Iomicron/external/include/ddk';
+export UNAME=Windows;
+
+# Build edbg
+make clean
+make
