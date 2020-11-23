fd05d58e7fb14371ce1c5679bcd2f59a5c06564e
ooxi <violetland@mail.ru>
2020-11-23 16:43:04 +0100

    GitHub Actions...

--- .github/workflows/linux-amd64-clang.sh
+++ .github/workflows/linux-amd64-clang.sh
@@ -9,7 +9,12 @@
 dnf install -y glibc-locale-source
 localedef  --force --inputfile=en_US --charmap=UTF-8 en_US.UTF-8
 
+# Install build dependencies
 sudo dnf -y install	\
 	clang		\
 	make		\
 	libudev-devel
+
+# Build edbg
+make
+ls -la
