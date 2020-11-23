10bd8b65598a844a4e6b34f80e6b404e1692a05b
ooxi <violetland@mail.ru>
2020-11-23 16:35:07 +0100

    GitHub Actions...

---
+++ .github/workflows/linux-amd64-clang.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Assumes to be executed with root permissions inside a Fedora
+# container.
+#
+# The following directories should be provided from the outside world
+#  - /github/workspace: read/write repository checkout
+
